0c6f3fd701ae18b2a413ced9d7342f79d75078ec
Alejandro Sanchez <alejandro.s1993@gmail.com>
2016-05-02 20:34:23 +0200

    Working project, fixed a selection mistake, and now works perfectly

--- projectMusic/MusicTableViewController.swift
+++ projectMusic/MusicTableViewController.swift
@@ -11,35 +11,48 @@ import UIKit
 class MusicTableViewController: UITableViewController {
 
     
-    var todos = [ "Learn swift", "Shoot table videos" , " Create music app",  "build iOs Apps"]
+    var years = [String]()
+    var identities = [String]()
+    var yearsInfo = [String]()
+    var pictures = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        years = [ "Music in the 1970", "Music in the 1980" , "Music in the 1990",  "Music in the 2000"]
+        identities = ["A","B", "C", "D"]
+        yearsInfo = ["in the 1970s", "in the 1980s", "in the 1990s", "in the 2000s"]
+        pictures = ["music-1","music-2", "music-3", "music-4"]
+        tableView.rowHeight = 150.0
+        tableView.backgroundColor = UIColor.blackColor()
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return todos.count
+        return years.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-        let todo = todos[indexPath.row]
-        // Configure the cell...
-        
-        cell.textLabel?.text = todo
-        
+        print(indexPath.row)
+        let year = years[indexPath.row]
+        cell.textLabel?.text = year
+        cell.detailTextLabel!.text = yearsInfo[indexPath.row]
+        let image : UIImage = UIImage(named: pictures[indexPath.row])!
+        cell.imageView?.image = image
+        cell.backgroundColor = UIColor.clearColor()
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let vcName = identities[indexPath.row]
+        let viewController = storyboard?.instantiateViewControllerWithIdentifier(vcName)
+        self.navigationController?.pushViewController(viewController!, animated: true)
     }
    
 }
